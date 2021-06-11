0fbbad7b0f4ce3c8cda2d60fa77d8df0e4c3d94b
xingcdev <60323726+xingcdev@users.noreply.github.com>
2021-06-11 15:34:21 +0200

    render todos on DOM

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## User stories
 
-- [ ] Add a todo when the user presses on the Enter key
-- [ ] There is a small grey rounded box on the left of a todo item
+- [X ] Add a todo when the user presses on the Enter key
+- [X ] There is a small grey rounded box on the left of a todo item
 - [ ] There is a red checkbox on the right of a todo item
 - [ ] Check the box to make a todo items completed.
 - [ ] When a todo item is completed, its background becomes grey and the checkbox will be blue
--- js/index.js
+++ js/index.js
@@ -5,9 +5,10 @@ const addTodo = function (text) {
 	const todo = {
 		id: Date.now(),
 		text: text,
-		completed: false,
+		isDone: false,
 	};
 	todos.push(todo);
+	renderTodoOnDOM(todo);
 };
 
 form.addEventListener('submit', (event) => {
@@ -24,6 +25,23 @@ form.addEventListener('submit', (event) => {
 });
 
 // TODO
-const renderTodo = function (todo) {
+const renderTodoOnDOM = function (todo) {
 	// Add a todo as li item on the DOM
+	const list = document.querySelector('.todo-list');
+
+	const node = document.createElement('li');
+	const isDone = todo.isDone ? 'isDone' : '';
+	node.setAttribute('class', `todo-item ${isDone}`);
+	node.setAttribute('data-id', todo.id);
+
+	node.innerHTML = `
+	<div class="right">
+		<input type="checkbox" id=${todo.id}> 
+		<label for=${todo.id}></label> 
+		<span class="text">${todo.text}</span> 
+	</div>
+	<a class="delete-todo js-delete-todo">x</a>`;
+
+	// Append node to the DOM
+	list.append(node);
 };
